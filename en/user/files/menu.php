d98794323518dbe76ebf0575b29bccce2cac7697
Diego Ronaldo <diego.ronaldosv@gmail.com>
2015-07-12 11:22:03 -0600

    Menu principal

--- en/user/files/menu.php
+++ en/user/files/menu.php
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Aero Control</a>
+      <a class="navbar-brand"href="index.php">Aero Control</a>
     </div>
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
--- es/airline/admin/nav.php
+++ es/airline/admin/nav.php
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#"><?=$row['name']?></a>
+      <a class="navbar-brand" href="index.php"><?=$row['name']?></a>
     </div>
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
