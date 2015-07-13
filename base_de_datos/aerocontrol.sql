29872d1a8dbb726421b2a95ce4399af1bc9e2274
Ted <tedd.bonilla@gmail.com>
2015-07-13 04:22:02 -0600

    asñdlak
    asldkas

--- base_de_datos/aerocontrol.sql
+++ base_de_datos/aerocontrol.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
+-- version 4.0.4
 -- http://www.phpmyadmin.net
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 09-07-2015 a las 00:10:57
--- Versión del servidor: 5.6.21
--- Versión de PHP: 5.6.3
+-- Servidor: localhost
+-- Tiempo de generación: 13-07-2015 a las 10:21:27
+-- Versión del servidor: 5.6.12-log
+-- Versión de PHP: 5.4.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `aerocontrol`
 --
+CREATE DATABASE IF NOT EXISTS `aerocontrol` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `aerocontrol`;
 
 -- --------------------------------------------------------
 
@@ -27,12 +29,13 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `admin` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user` varchar(50) NOT NULL,
   `password` varchar(32) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `phone` varchar(9) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `phone` varchar(9) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Volcado de datos para la tabla `admin`
@@ -48,12 +51,14 @@ INSERT INTO `admin` (`id`, `user`, `password`, `name`, `phone`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `aircraft` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `airline` int(11) NOT NULL,
   `seats` int(11) NOT NULL,
-  `type` varchar(50) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+  `type` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `airline` (`airline`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Volcado de datos para la tabla `aircraft`
@@ -74,11 +79,12 @@ INSERT INTO `aircraft` (`id`, `name`, `airline`, `seats`, `type`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `airlines` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `description` varchar(300) NOT NULL,
-  `logo` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+  `logo` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Volcado de datos para la tabla `airlines`
@@ -100,10 +106,12 @@ INSERT INTO `airlines` (`id`, `name`, `description`, `logo`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `airports` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
-  `location` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+  `location` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `location` (`location`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
 
 --
 -- Volcado de datos para la tabla `airports`
@@ -131,11 +139,22 @@ INSERT INTO `airports` (`id`, `name`, `location`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `bookings` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `costumer` int(11) NOT NULL,
   `flight` int(11) NOT NULL,
-  `seats` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `seats` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `flight` (`flight`),
+  KEY `costumer` (`costumer`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Volcado de datos para la tabla `bookings`
+--
+
+INSERT INTO `bookings` (`id`, `costumer`, `flight`, `seats`) VALUES
+(1, 2, 7, 1),
+(2, 2, 11, 1);
 
 -- --------------------------------------------------------
 
@@ -144,11 +163,12 @@ CREATE TABLE IF NOT EXISTS `bookings` (
 --
 
 CREATE TABLE IF NOT EXISTS `cities` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `city` varchar(50) NOT NULL,
   `state` varchar(50) NOT NULL,
-  `zip` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+  `zip` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
 
 --
 -- Volcado de datos para la tabla `cities`
@@ -156,12 +176,12 @@ CREATE TABLE IF NOT EXISTS `cities` (
 
 INSERT INTO `cities` (`id`, `city`, `state`, `zip`) VALUES
 (1, 'San Salvador', 'El Salvador', 503),
-(2, 'San Francisco', 'Estados Unidos', 1009),
+(2, 'San Francisco', 'San Francisco', 1009),
 (3, 'Buenos Aires', 'Argentina', 54),
 (4, 'La Paz', 'Bolivia', 591),
 (5, 'RÃ­o de Janeiro', 'Brasil', 55),
 (6, 'BogotÃ¡', 'Colombia', 57),
-(7, 'Guayaquil', 'Ecuador', 593),
+(7, 'BogotÃ¡', 'Ecuador', 593),
 (8, 'AsunciÃ³n', 'Paraguay', 595),
 (9, 'Lima', 'PerÃº', 51),
 (10, 'Montevideo', 'Uruguay', 598),
@@ -178,7 +198,7 @@ INSERT INTO `cities` (`id`, `city`, `state`, `zip`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `costumers` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `address` varchar(100) NOT NULL,
   `city` varchar(100) NOT NULL,
@@ -188,8 +208,9 @@ CREATE TABLE IF NOT EXISTS `costumers` (
   `phone` varchar(9) NOT NULL,
   `user` varchar(50) NOT NULL,
   `password` varchar(32) NOT NULL,
-  `status` char(1) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `status` char(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Volcado de datos para la tabla `costumers`
@@ -206,7 +227,7 @@ INSERT INTO `costumers` (`id`, `name`, `address`, `city`, `state`, `mail`, `birt
 --
 
 CREATE TABLE IF NOT EXISTS `flights` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `airline` int(11) NOT NULL,
   `departure_time` datetime NOT NULL,
   `departure_city` int(11) NOT NULL,
@@ -217,21 +238,31 @@ CREATE TABLE IF NOT EXISTS `flights` (
   `arrival_runway` int(11) NOT NULL,
   `cost` double NOT NULL,
   `seats` int(4) NOT NULL,
-  `description` varchar(500) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+  `description` varchar(500) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `airline` (`airline`),
+  KEY `aircraft` (`aircraft`),
+  KEY `departure_city` (`departure_city`),
+  KEY `arrival_city` (`arrival_city`),
+  KEY `aircraft_2` (`aircraft`),
+  KEY `departure_runway` (`departure_runway`),
+  KEY `arrival_runway` (`arrival_runway`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
 --
 -- Volcado de datos para la tabla `flights`
 --
 
 INSERT INTO `flights` (`id`, `airline`, `departure_time`, `departure_city`, `arrival_time`, `arrival_city`, `aircraft`, `departure_runway`, `arrival_runway`, `cost`, `seats`, `description`) VALUES
-(1, 1, '2015-07-26 10:30:00', 15, '2015-07-25 14:30:00', 14, 1, 1, 2, 300, 250, 'nvhvhjgvjh'),
-(2, 6, '2015-07-30 21:00:00', 13, '2015-07-30 23:30:00', 11, 5, 1, 1, 250, 300, 'Tacos'),
-(6, 5, '2015-07-26 19:30:00', 9, '2015-07-26 22:30:00', 12, 3, 1, 1, 350, 250, 'khghjfj'),
-(7, 4, '2015-07-30 20:00:00', 4, '2015-07-30 23:00:00', 10, 4, 1, 1, 500, 100, 'Aqui'),
-(8, 6, '2015-07-28 09:00:00', 3, '2015-07-28 13:00:00', 14, 5, 1, 1, 750, 300, 'Argentina'),
-(9, 6, '2015-07-29 10:00:00', 10, '2015-07-29 12:00:00', 15, 5, 1, 2, 400, 200, 'Uruguay'),
-(10, 1, '2015-07-28 17:00:00', 14, '2015-07-28 18:00:00', 7, 1, 1, 1, 550, 150, 'Esto');
+(1, 1, '2015-06-25 16:30:00', 15, '2015-06-25 14:30:00', 14, 1, 1, 2, 300, 250, 'nvhvhjgvjh'),
+(2, 6, '2015-06-30 21:00:00', 13, '2015-06-30 11:00:00', 11, 5, 1, 1, 250, 300, 'Tacos'),
+(6, 5, '2015-06-26 19:30:00', 9, '2015-06-26 12:30:00', 12, 3, 1, 1, 350, 250, 'khghjfj'),
+(7, 4, '2015-06-30 10:00:00', 4, '2015-06-23 08:00:00', 10, 4, 1, 1, 500, 99, 'Aqui'),
+(8, 6, '2015-06-28 19:00:00', 3, '2015-06-28 13:00:00', 14, 5, 1, 1, 750, 300, 'Argentina'),
+(9, 6, '2015-06-29 10:00:00', 10, '2015-06-29 05:00:00', 15, 5, 1, 2, 400, 200, 'Uruguay'),
+(10, 1, '2015-06-27 17:00:00', 14, '2015-06-28 13:00:00', 7, 1, 1, 1, 550, 150, 'Esto'),
+(11, 1, '2015-07-14 21:00:00', 1, '2015-07-15 10:31:00', 12, 1, 1, 1, 600, 299, 'Mejor vuelo a donde desees'),
+(12, 1, '2015-07-31 13:00:00', 12, '2015-07-29 06:31:00', 1, 1, 2, 1, 600, 300, 'LOL');
 
 -- --------------------------------------------------------
 
@@ -240,10 +271,12 @@ INSERT INTO `flights` (`id`, `airline`, `departure_time`, `departure_city`, `arr
 --
 
 CREATE TABLE IF NOT EXISTS `runways` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `idairport` int(11) NOT NULL,
-  `length` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `length` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idairport` (`idairport`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Volcado de datos para la tabla `runways`
@@ -260,15 +293,16 @@ INSERT INTO `runways` (`id`, `idairport`, `length`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `user-airline` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `phone` int(11) NOT NULL,
   `user` varchar(50) NOT NULL,
   `password` varchar(32) NOT NULL,
   `status` char(1) NOT NULL,
   `airline` int(11) NOT NULL,
-  `type` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+  `type` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Volcado de datos para la tabla `user-airline`
@@ -284,124 +318,6 @@ INSERT INTO `user-airline` (`id`, `name`, `phone`, `user`, `password`, `status`,
 (7, 'AeroMexico', 23457432, 'AdminAero', '4297f44b13955235245b2497399d7a93', '1', 7, 1);
 
 --
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `admin`
---
-ALTER TABLE `admin`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `user` (`user`);
-
---
--- Indices de la tabla `aircraft`
---
-ALTER TABLE `aircraft`
- ADD PRIMARY KEY (`id`), ADD KEY `airline` (`airline`);
-
---
--- Indices de la tabla `airlines`
---
-ALTER TABLE `airlines`
- ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `airports`
---
-ALTER TABLE `airports`
- ADD PRIMARY KEY (`id`), ADD KEY `location` (`location`);
-
---
--- Indices de la tabla `bookings`
---
-ALTER TABLE `bookings`
- ADD PRIMARY KEY (`id`), ADD KEY `flight` (`flight`), ADD KEY `costumer` (`costumer`);
-
---
--- Indices de la tabla `cities`
---
-ALTER TABLE `cities`
- ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `costumers`
---
-ALTER TABLE `costumers`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `mail` (`mail`), ADD UNIQUE KEY `user` (`user`);
-
---
--- Indices de la tabla `flights`
---
-ALTER TABLE `flights`
- ADD PRIMARY KEY (`id`), ADD KEY `airline` (`airline`), ADD KEY `aircraft` (`aircraft`), ADD KEY `departure_city` (`departure_city`), ADD KEY `arrival_city` (`arrival_city`), ADD KEY `aircraft_2` (`aircraft`), ADD KEY `departure_runway` (`departure_runway`), ADD KEY `arrival_runway` (`arrival_runway`);
-
---
--- Indices de la tabla `runways`
---
-ALTER TABLE `runways`
- ADD PRIMARY KEY (`id`), ADD KEY `idairport` (`idairport`);
-
---
--- Indices de la tabla `user-airline`
---
-ALTER TABLE `user-airline`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `user` (`user`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `admin`
---
-ALTER TABLE `admin`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT de la tabla `aircraft`
---
-ALTER TABLE `aircraft`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
---
--- AUTO_INCREMENT de la tabla `airlines`
---
-ALTER TABLE `airlines`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
---
--- AUTO_INCREMENT de la tabla `airports`
---
-ALTER TABLE `airports`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
---
--- AUTO_INCREMENT de la tabla `bookings`
---
-ALTER TABLE `bookings`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `cities`
---
-ALTER TABLE `cities`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
---
--- AUTO_INCREMENT de la tabla `costumers`
---
-ALTER TABLE `costumers`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT de la tabla `flights`
---
-ALTER TABLE `flights`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
---
--- AUTO_INCREMENT de la tabla `runways`
---
-ALTER TABLE `runways`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT de la tabla `user-airline`
---
-ALTER TABLE `user-airline`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
---
 -- Restricciones para tablas volcadas
 --
 
@@ -409,37 +325,37 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 -- Filtros para la tabla `aircraft`
 --
 ALTER TABLE `aircraft`
-ADD CONSTRAINT `aircraft_ibfk_1` FOREIGN KEY (`airline`) REFERENCES `airlines` (`id`);
+  ADD CONSTRAINT `aircraft_ibfk_1` FOREIGN KEY (`airline`) REFERENCES `airlines` (`id`);
 
 --
 -- Filtros para la tabla `airports`
 --
 ALTER TABLE `airports`
-ADD CONSTRAINT `airports_ibfk_1` FOREIGN KEY (`location`) REFERENCES `cities` (`id`);
+  ADD CONSTRAINT `airports_ibfk_1` FOREIGN KEY (`location`) REFERENCES `cities` (`id`);
 
 --
 -- Filtros para la tabla `bookings`
 --
 ALTER TABLE `bookings`
-ADD CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`flight`) REFERENCES `flights` (`id`),
-ADD CONSTRAINT `bookings_ibfk_2` FOREIGN KEY (`costumer`) REFERENCES `costumers` (`id`);
+  ADD CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`flight`) REFERENCES `flights` (`id`),
+  ADD CONSTRAINT `bookings_ibfk_2` FOREIGN KEY (`costumer`) REFERENCES `costumers` (`id`);
 
 --
 -- Filtros para la tabla `flights`
 --
 ALTER TABLE `flights`
-ADD CONSTRAINT `flights_ibfk_1` FOREIGN KEY (`airline`) REFERENCES `airlines` (`id`),
-ADD CONSTRAINT `flights_ibfk_2` FOREIGN KEY (`departure_city`) REFERENCES `cities` (`id`),
-ADD CONSTRAINT `flights_ibfk_3` FOREIGN KEY (`arrival_city`) REFERENCES `cities` (`id`),
-ADD CONSTRAINT `flights_ibfk_4` FOREIGN KEY (`aircraft`) REFERENCES `aircraft` (`id`),
-ADD CONSTRAINT `flights_ibfk_5` FOREIGN KEY (`departure_runway`) REFERENCES `runways` (`id`),
-ADD CONSTRAINT `flights_ibfk_6` FOREIGN KEY (`arrival_runway`) REFERENCES `runways` (`id`);
+  ADD CONSTRAINT `flights_ibfk_1` FOREIGN KEY (`airline`) REFERENCES `airlines` (`id`),
+  ADD CONSTRAINT `flights_ibfk_2` FOREIGN KEY (`departure_city`) REFERENCES `cities` (`id`),
+  ADD CONSTRAINT `flights_ibfk_3` FOREIGN KEY (`arrival_city`) REFERENCES `cities` (`id`),
+  ADD CONSTRAINT `flights_ibfk_4` FOREIGN KEY (`aircraft`) REFERENCES `aircraft` (`id`),
+  ADD CONSTRAINT `flights_ibfk_5` FOREIGN KEY (`departure_runway`) REFERENCES `runways` (`id`),
+  ADD CONSTRAINT `flights_ibfk_6` FOREIGN KEY (`arrival_runway`) REFERENCES `runways` (`id`);
 
 --
 -- Filtros para la tabla `runways`
 --
 ALTER TABLE `runways`
-ADD CONSTRAINT `runways_ibfk_1` FOREIGN KEY (`idairport`) REFERENCES `airports` (`id`);
+  ADD CONSTRAINT `runways_ibfk_1` FOREIGN KEY (`idairport`) REFERENCES `airports` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
